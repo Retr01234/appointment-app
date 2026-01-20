fd2e1818c028ba350493c37ec8c242ebdea280b8
pavea <pave.ahmad@adesso.de>
2026-01-20 15:11:18 +0100

    feature: add 2 way data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppointmentList } from "./appointment-list/appointment-list";
+import { AppointmentList } from './appointment-list/appointment-list';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, AppointmentList],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('appointment-app');
--- src/app/appointment-list/appointment-list.ts
+++ src/app/appointment-list/appointment-list.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
-import { App } from '../app';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment-list',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './appointment-list.html',
   styleUrl: './appointment-list.css',
 })
+
 export class AppointmentList {
-  appointment: Appointment = { id: 1, title: 'Dentist Appointment', date: new Date('2026-01-11') };
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
 }
