dec04196b77f2d07c7d12b1165a990beec73cd3f
pavea <pave.ahmad@adesso.de>
2026-01-20 14:31:05 +0100

    feature: add new Model

--- src/app/appointment-list/appointment-list.ts
+++ src/app/appointment-list/appointment-list.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+import { App } from '../app';
 
 @Component({
   selector: 'app-appointment-list',
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './appointment-list.css',
 })
 export class AppointmentList {
-
+  appointment: Appointment = { id: 1, title: 'Dentist Appointment', date: new Date('2026-01-11') };
 }
